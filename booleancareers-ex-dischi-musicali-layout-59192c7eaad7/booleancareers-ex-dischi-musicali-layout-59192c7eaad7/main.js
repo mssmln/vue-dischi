8469e358f94b9b96b63f7aa50b708bc63c6f0e68
Massimiliano <m.croci.ly@gmail.com>
2021-02-09 15:03:38 +0100

    init

---
+++ booleancareers-ex-dischi-musicali-layout-59192c7eaad7/booleancareers-ex-dischi-musicali-layout-59192c7eaad7/main.js
@@ -0,0 +1,37 @@
+// Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali.
+// Stampiamo tutto a schermo, in questo momento non è importante la parte grafica.
+// Bonus:
+// Creare una select con i seguenti generi: pop, rock, metal e jazz.
+// In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+// Chiamata: https://flynn.boolean.careers/exercises/api/array/music
+// Layout base:
+// https://bitbucket.org/booleancareers/ex-dischi-musicali-layout
+// Togliete $(document).ready(...) dal js
+
+
+var app = new Vue ({
+  el: '#app',
+  data: {
+    dischi: [],
+    generi: []
+  },
+  mounted(){
+    for(let i = 0; i < 10; i++){
+      axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((result) => {
+        // console.log(result.data.response);
+        this.dischi = result.data.response;
+        // console.log(this.dischi[i].title);
+
+        // popoliamo con i generi
+        this.dischi.forEach((item) => {
+          (!this.generi.includes(item.genre)) ? this.generi.push(item.genre) : '';
+        });
+      });
+      // .catch()
+
+    }
+    console.log(this.generi);
+  }
+});
